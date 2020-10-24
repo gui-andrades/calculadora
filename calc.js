177dcb58fed49a7d2f25ac026dea995e4772ba6f
Bernola <Bernola@users.noreply.github.com>
2020-10-24 18:22:55 -0300

    Finishing merge fail resolution

---
+++ calc.js
@@ -0,0 +1,66 @@
+var firstInput=0
+var secondInput=0
+var calcType=-1
+var result=-1
+var lastWasEquals=false
+var lastWasOp=false
+
+function clean(){
+    lastWasEquals=false
+    document.getElementById('display').value=''
+}
+
+function decideOperation(type){
+    firstInput=parseFloat(document.getElementById('display').value)
+    calcType=type
+    lastWasEquals=false
+    lastWasOp=true
+}
+
+function calc(ct, n1, n2){
+    if (ct==1){
+        return (n1/n2)
+    }
+    if (ct==2){
+        return (n1*n2)
+    }
+    if (ct==3){
+        return (n1-n2)
+    }
+    if (ct==4){
+        return (n1+n2)
+    }
+}
+
+function equals(){
+    if(lastWasEquals){
+        firstInput=result
+        result = calc(calcType,firstInput,secondInput)
+        document.getElementById('display').value=result
+        return
+    }
+    secondInput=parseFloat(document.getElementById('display').value)
+    if (calcType==-1){
+        return
+    }
+    result = calc(calcType,firstInput,secondInput)
+    document.getElementById('display').value=result
+    lastWasEquals=true
+    lastWasOp=false
+}
+
+function type(val){
+    if(lastWasOp){
+        lastWasOp=false
+        clean()
+    }
+    if(lastWasEquals){
+        lastWasEquals=false
+        result=-1
+        firstInput=0
+        secondInput=0
+        calcType=-1
+        clean()
+    }
+    document.getElementById('display').value+=val
+}
